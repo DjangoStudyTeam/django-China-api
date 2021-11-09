5bdb2ad61779a6532be014d6f9d330064be0887e
jkc <47583701+s649821022@users.noreply.github.com>
2021-11-09 09:34:39 +0800

    新增登录接口 (#8)
    * 新增登录接口

--- api/apps/users/serializers.py
+++ api/apps/users/serializers.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
 from djoser.serializers import UserCreatePasswordRetypeSerializer
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 from .models import User
 
@@ -14,3 +17,37 @@ class UserSerializer(serializers.ModelSerializer):
 
 class RegisterSerializer(UserCreatePasswordRetypeSerializer):
     email = serializers.EmailField()
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(
+        label=_("Username"),
+    )
+    password = serializers.CharField(
+        label=_("Password"),
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True
+    )
+
+    def validate(self, attrs):
+        username = attrs["username"]
+        password = attrs["password"]
+
+        user = authenticate(
+            request=self.context.get("request"),
+            username=username, password=password
+        )
+        if not user:
+            msg = _("Unable to login with provided credentials.")
+            raise serializers.ValidationError(msg)
+        attrs['user'] = user
+        return attrs
+
+
+class TokenSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Token
+        fields = ["key", "user"]
--- api/apps/users/views.py
+++ api/apps/users/views.py
@@ -1,13 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.signals import user_logged_in
 from djoser.email import ActivationEmail
 from drf_spectacular.utils import extend_schema
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-
 from . import signals
-from .serializers import RegisterSerializer, UserSerializer
+from .serializers import RegisterSerializer, UserSerializer, LoginSerializer, TokenSerializer
+
+
+User = get_user_model()
 
 
 class AuthViewSet(viewsets.GenericViewSet):
@@ -19,6 +24,8 @@ class AuthViewSet(viewsets.GenericViewSet):
     def get_serializer_class(self):
         if self.action == "register":
             return RegisterSerializer
+        elif self.action == "login":
+            return LoginSerializer
         return super().get_serializer_class()
 
     @extend_schema(
@@ -46,4 +53,31 @@ class AuthViewSet(viewsets.GenericViewSet):
         return Response(
             data=output_serializer.data,
             status=status.HTTP_201_CREATED,
+        )
+
+    @extend_schema(
+        summary="Login",
+        request=LoginSerializer,
+        responses={200: TokenSerializer},
+    )
+    @action(
+        ["POST"],
+        detail=False,
+        url_name="login",
+        url_path="login",
+        serializer_class=TokenSerializer,
+    )
+    def login(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        Token.objects.filter(user=user).delete()
+        token, _ = Token.objects.get_or_create(user=user)
+        user_logged_in.send(
+            sender=self.__class__, user=user, request=self.request
+        )
+        output_serializer = TokenSerializer(token, context={"request": request})
+        return Response(
+            data=output_serializer.data,
+            status=status.HTTP_200_OK
         )
