b7a5036703fc143e3f4b7ff2e19f5c7b0575663e
s649821022 <1044500650@qq.com>
2021-12-15 10:20:22 +0800

    修复black格式不正确

--- api/apps/nodes/apps.py
+++ api/apps/nodes/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NodesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nodes'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "nodes"
