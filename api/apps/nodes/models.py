8a94688373d1bf9c5348226bb53f83e75f4e287e
s649821022 <1044500650@qq.com>
2021-12-17 14:17:28 +0800

    修改nodes模型

--- api/apps/nodes/models.py
+++ api/apps/nodes/models.py
@@ -3,26 +3,15 @@ import os
 from core.models import TimeStampedModel
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from tree_queries.fields import TreeNodeForeignKey
+from tree_queries.models import TreeNode
 
 
-def note_cover_path(instance, filename):
-    return os.path.join("notes", "cover", instance.name, filename)
+def node_cover_path(instance, filename):
+    return os.path.join("nodes", "cover", instance.name, filename)
 
 
-class Notes(TimeStampedModel):
-    parent = TreeNodeForeignKey(
-        "self",
-        blank=True,
-        null=True,
-        on_delete=models.CASCADE,
-        verbose_name=_("parent"),
-        related_name="children",
-    )
+class Notes(TimeStampedModel, TreeNode):
     name = models.CharField(_("name"), max_length=30)
     slug = models.SlugField(_("slug"), allow_unicode=True)
-    cover = models.ImageField(_("cover"), upload_to=note_cover_path, blank=True)
-    description = models.CharField(_("description"), max_length=100)
-
-    class Meta(TimeStampedModel.Meta):
-        pass
+    cover = models.ImageField(_("cover"), upload_to=node_cover_path, blank=True)
+    description = models.TextField(_("description"), blank=True)
