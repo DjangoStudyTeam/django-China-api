4857998f885d53a0ee6f541dabacb9296aa06e62
jkc <47583701+s649821022@users.noreply.github.com>
2021-12-28 09:29:41 +0800

    创建posts模型 (#29)
    * 创建posts模型

---
+++ api/apps/posts/models.py
@@ -0,0 +1,20 @@
+from core.models import TimeStampedModel
+from django.conf import settings
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils.fields import MonitorField
+from nodes.models import Node
+
+
+class Post(TimeStampedModel):
+    title = models.CharField(_("title"), max_length=150)
+    body = models.TextField(_("body"), blank=True)
+    views = models.IntegerField(_("views"), default=0)
+    pinned = models.BooleanField(_("pinned"), default=False)
+    highlighted = models.BooleanField(_("highlighted"), default=False)
+    deleted = models.BooleanField(_("deleted"), default=False)
+    edited_at = MonitorField(_("edited_at"), monitor=body)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="user", related_name="posts"
+    )
+    node = models.ForeignKey(Node, on_delete=models.CASCADE, verbose_name="node", related_name="posts")
